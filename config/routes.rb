3cc551935c3fad11cff1ea5bd4ce70b41e7d27df
nico alavi <alavinic@gmail.com>
2024-12-21 16:10:01 +0900

    refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 
-  # reflects urls like /tasks/1 where :id represents unique task identifier
-  # to tasks show routes requests to show action in tasks controller
-  # as tasks creates general URL for specific task
+  # replaces individual routes with resources for tasks
+  # automatically generates all necessary routes for CRUD actions
+  resources :tasks
 end
