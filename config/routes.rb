5146715962c658b05117e629f5fc2d0f49f252b1
nico alavi <alavinic@gmail.com>
2024-12-21 14:12:20 +0900

    adjust formatting in new task submission.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
   # reflects urls like /tasks/1 where :id represents unique task identifier
   # to tasks show routes requests to show action in tasks controller
