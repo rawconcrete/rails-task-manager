058c5b3989496c8b92deac9b03bc68dcc41d85ba
nico alavi <alavinic@gmail.com>
2024-12-21 14:53:56 +0900

    adjust routes for task editing.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
   # reflects urls like /tasks/1 where :id represents unique task identifier
   # to tasks show routes requests to show action in tasks controller
   # as tasks creates general URL for specific task
